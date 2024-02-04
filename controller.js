e1e88f1a7c5a59f90503185e8603de9ce2e82733
13acid <chris.andon+github@gmail.com>
2024-02-04 01:45:50 +0000

    update controller function to allow for multiple simultaneous key presses (move diagonally)

--- controller.js
+++ controller.js
@@ -1,17 +1,18 @@
 const controls = {
   start: function(){
-  window.addEventListener('keydown', function (e) {
-    controls.key = e.keyCode;
-  })
-  window.addEventListener('keyup', function (e) {
-    controls.key = false;
-  })
+    controls.keys = [];
+    window.addEventListener('keydown', function (e) {
+      controls.keys[e.keyCode] = true;
+    })
+    window.addEventListener('keyup', function (e) {
+      controls.keys[e.keyCode] = false;
+    })
   }
 }
 
 function updatePlayer(){
-    if(controls.key && controls.key == 37) {player.x = player.x - 1;} 
-    if(controls.key && controls.key == 39) {player.x = player.x + 1;} 
-    if(controls.key && controls.key == 38) {player.y = player.y - 1;} 
-    if(controls.key && controls.key == 40) {player.y = player.y + 1;} 
+    if(controls.keys[37]) {player.x = player.x - 1;} 
+    if(controls.keys[39]) {player.x = player.x + 1;} 
+    if(controls.keys[38]) {player.y = player.y - 1;} 
+    if(controls.keys[40]) {player.y = player.y + 1;} 
 }
